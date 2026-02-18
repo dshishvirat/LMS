5db6a1c2d17e948343f99f7eaee363e502d90698
Dev Ashish <dshish719@gmail.com>
2026-02-18 11:19:32 +0530

    Fix CORS origin to include https protocol

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser())
 
 app.use(cors({
-    origin:"lms-seven-ebon-93.vercel.app",
+    origin:"https://lms-seven-ebon-93.vercel.app",
     credentials:true
 }))
 
